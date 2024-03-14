7f3db9dcf1ef85b876a94ee531dbf598bedfc0d2
amadoracacioperez <155078689+amadoracacioperez@users.noreply.github.com>
2024-03-14 12:00:58 +0000

    Add files via upload

---
+++ Public/Projects/+18/index.js
@@ -0,0 +1,27 @@
+//IF STATMENTS
+const myText = document.getElementById("myText");
+const submitBtn = document.getElementById("myButton");
+const answer = document.getElementById("answer");
+
+submitBtn.onclick = function(){
+    age = myText.value;
+    age = Number(age);
+    if(age <= 17){
+        answer.textContent = `You are too young for this website`
+    }
+    else if(age >=18 && age <24){
+        answer.textContent = `You are not older enough to see this content. Try next year`
+    }
+    else if(age == 24){
+        answer.textContent = `You have the perfect age for this site`
+    }
+    else if(age > 24 && age < 90){
+        answer.textContent = `You are too old for this site`
+    }
+    else if(age > 90){
+        answer.textContent = `Be careful! You can suffer a heart attack.`
+    }
+    else{
+        answer.textContent = `Access denied.`
+    }
+}
