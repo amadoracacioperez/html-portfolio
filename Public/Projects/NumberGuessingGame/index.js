db39c9e488baf61ae4d494b90055381eef28240a
amadoracacioperez <155078689+amadoracacioperez@users.noreply.github.com>
2024-03-21 11:36:51 +0000

    Add files via upload

---
+++ Public/Projects/NumberGuessingGame/index.js
@@ -0,0 +1,39 @@
+
+const min = 1;
+const max = 100;
+const randomNumber = Math.floor(Math.random()*(max - min + 1));
+
+let guess;
+let attemps = 0;
+let running = true;
+
+while(running){
+
+    window.prompt(`Guess a number between ${min} - ${max}:`);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert(`Please enter a valid number:`);
+    }
+
+    else if(guess < min || guess > max){
+        window.alert(`Please enter a valid number:`);
+    }
+
+    else{
+        attemps++;
+        if(guess < randomNumber){
+            window.alert("Too low! Try again!");
+        }
+        else if(guess > randomNumber){
+            window.alert("Too High! Try again!");
+        }
+        else{
+            window.alert(`CORRECT! The answer was ${randomNumber}. It took you ${attemps} attemps`);
+            running = false;
+        }
+
+    }
+}
+
+
