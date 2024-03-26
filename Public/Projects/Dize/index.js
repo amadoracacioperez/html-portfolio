94229be59559f07577dfff88f8a82bc40ec8da21
amadoracacioperez <155078689+amadoracacioperez@users.noreply.github.com>
2024-03-26 10:53:49 +0000

    Add files via upload

--- Public/Projects/Dize/index.js
+++ Public/Projects/Dize/index.js
@@ -1,7 +1,20 @@
 
-let username;
+function roll(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImage = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+    
+    for( i = 0; i < numOfDice; i++){
+        randNumber = Math.floor(Math.random()* 6) +1;
+        values.push(randNumber);
+        images.push(`<img src="ImagesDize/${randNumber}.jpg">`);
+        console.log(values)
+    }
 
-document.getElementById("MySubmit").onclick = function(){
-    username = document.getElementById("MyText").value ;
-    document.getElementById("MyH2").textContent = `Welcome ${username}`;
+    diceResult.textContent = `dice: ${values.join(' , ')}`;
+    diceImage.innerHTML = images.join(" ");
+
+
 }
