ea627a7fe96b09a8fcb4bc54d20b2ba2587b8eb1
amadoracacioperez <155078689+amadoracacioperez@users.noreply.github.com>
2024-03-26 10:34:01 +0000

    Add files via upload

--- Public/Projects/Dize/index.js
+++ Public/Projects/Dize/index.js
@@ -1,12 +1,7 @@
-const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
 
-const min = 1;
-const max = 6;
-let randNumber; 
+let username;
 
-myButton.onclick = function(){
-    randNumber = Math.floor(Math.random()* max) + min;
-    myLabel.textContent = randNumber;
-
+document.getElementById("MySubmit").onclick = function(){
+    username = document.getElementById("MyText").value ;
+    document.getElementById("MyH2").textContent = `Welcome ${username}`;
 }
